cdaee1851966c73c01a60be36e9d682091109f33
GodfatherTHR <156956145+GodfatherTHR@users.noreply.github.com>
2024-03-13 22:05:57 +0600

    Update wp.py

--- wp.py
+++ wp.py
@@ -1,20 +1,9 @@
-# import pyautogui as gui
-# import time
-# message = input("Enter your message: ")
-# number = input("Enter your number: ")
+import pyautogui as gui
+import time
+message = input("Enter your message: ")
+number = input("Enter your number: ")
 
-# time.sleep(2)
-# for i in range(int(number)):
-#     gui.typewrite(message)
-#     gui.press('Enter')
-nem = ["Titans", "Rovers", "Army", "Navy", "Zombie"]
-# nem_modify= nem.upper()
-nem[2]= "Warriors"
-nem.remove("Zombie")
-print(nem)
-fruits = {"apple", "banana", "cherry"}
-x, y, z = fruits
-
-print(x)
-print(type(y))
-print(z)
+time.sleep(2)
+for i in range(int(number)):
+    gui.typewrite(message)
+    gui.press('Enter')
